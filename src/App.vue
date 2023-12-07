17fae5d04a1b35890de4ccca26a7e74a63cef975
Pierre Freire <pierrefreire.profis@gmail.com>
2023-12-07 15:54:24 -0300

    Fonts added

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavbarComponent />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavbarComponent from './components/NavbarComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavbarComponent,
   }
 }
 </script>
 
 <style>
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+
+html, body, #app {
+  height: 100%;
+}
+
+@font-face {
+    font-family: 'Latino';
+    src: url('~@/assets/fonts/Latinotype-TrendSansFive.otf');
+}
+
+@font-face {
+    font-family: 'Brandon';
+    src: url('~@/assets/fonts/BrandonGrotesqueBlack.otf');
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #074A75;
 }
 </style>
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav>
+    <h1 class="title">Participe</h1>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavbarComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+.title {
+  color: white;
+  font-size: 30px;
+}
+</style>
